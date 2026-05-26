6bcc47f1413d46d5e64a562138c5d6913c954c4c
densid <dnl.martemyanov@gmail.com>
2026-05-26 18:39:41 +0300

    Добавил задания, дополнил темплейты

--- init/outputs.tf
+++ init/outputs.tf
@@ -3,3 +3,7 @@ output "cluster_url" {
   value = "https://console.yandex.cloud/folders/${var.folder_id}/managed-kubernetes/cluster/${yandex_kubernetes_cluster.k8s_cluster.id}/overview"
 }
 
+output "cluster_connection" {
+  description = "Kubernetes cluster URL"
+  value = "yc managed-kubernetes cluster get-credentials --id ${yandex_kubernetes_cluster.k8s_cluster.id} --external"
+}
